3b4af0e0b9a210cb2e5cdfae2b33110977d828ec
EchenDesktop <ericc@ERICDESKTOP-PC>
2024-06-19 16:48:28 -0600

    first commit

---
+++ board/pages.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+bp = Blueprint("pages", __name__)
+
+@bp.route("/")
+def home():
+    return render_template("pages/dashboard.html")
+
+@bp.route("/about")
+def about():
+    return render_template("pages/about.html")
