5d3d70ce0a34758f8a4be9d7c3e35e41ceca06f7
EchenDesktop <ericc@ERICDESKTOP-PC>
2024-06-19 17:30:57 -0600

    trying something else

--- board/__init__.py
+++ board/__init__.py
@@ -2,8 +2,10 @@ from flask import Flask
 
 from board import pages
 
+def create_app():
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    app.register_blueprint(pages.bp)
+    return app
 
-app.register_blueprint(pages.bp)
-#return app
+create_app()
