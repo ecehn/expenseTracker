bb7102ac5b97a33443c216cc31469646313efd28
EchenDesktop <ericc@ERICDESKTOP-PC>
2024-06-20 08:59:16 -0600

    renaming

--- board/pages.py
+++ board/pages.py
@@ -7,12 +7,12 @@ def dashboard():
     return render_template("pages/dashboard.html")
 
 @bp.route("/create")
-def createTrip():
-    return render_template("pages/createTrip.html")
+def createEvent():
+    return render_template("pages/createEvent.html")
 
 @bp.route("/view")
 def viewAndEdit():
-    return render_template("pages/viewEditTrip.html")
+    return render_template("pages/viewEditEvent.html")
 
 @bp.route("/account")
 def userAccount():
