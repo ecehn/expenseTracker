ec40d290fde6a18e63b1ac7dc6b6616a86e2f80e
EchenDesktop <ericc@ERICDESKTOP-PC>
2024-06-20 08:48:12 -0600

    Added pages in nav

--- board/pages.py
+++ board/pages.py
@@ -3,9 +3,17 @@ from flask import Blueprint, render_template
 bp = Blueprint("pages", __name__)
 
 @bp.route("/")
-def home():
+def dashboard():
     return render_template("pages/dashboard.html")
 
-@bp.route("/about")
-def about():
-    return render_template("pages/about.html")
+@bp.route("/create")
+def createTrip():
+    return render_template("pages/createTrip.html")
+
+@bp.route("/view")
+def viewAndEdit():
+    return render_template("pages/viewEditTrip.html")
+
+@bp.route("/account")
+def userAccount():
+    return render_template("pages/userAccount.html")
