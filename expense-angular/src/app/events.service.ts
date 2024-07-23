22c3fd46b87a15a487a217437307d87100a9b250
EchenDesktop <ericc@ERICDESKTOP-PC>
2024-07-23 11:52:51 -0600

    updated environment variables

--- expense-angular/src/app/events.service.ts
+++ expense-angular/src/app/events.service.ts
@@ -1,13 +1,13 @@
-// src/app/events.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
-  private apiUrl = 'http://127.0.0.1:8000/api/events';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
---
+++ expense-angular/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://expense-tracker-rho-beige.vercel.app/api/events'
+  };
+  
