2cba900a019904a231ccda7a1a836ceeb9aa6ec5
EchenDesktop <ericc@ERICDESKTOP-PC>
2024-07-24 12:19:58 -0600

    changed prod URL

--- expense-angular/src/environments/environment.development.ts
+++ expense-angular/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://127.0.0.1:8000/api/events'
+    apiUrl: 'https://jsonplaceholder.typicode.com/posts/1'
+    //apiUrl: 'http://127.0.0.1:8000/api/events'
   };
   
