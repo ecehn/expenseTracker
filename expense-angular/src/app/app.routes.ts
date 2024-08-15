9b86ff809e6549a0864b706f4b4cbfe5467d4b56
EchenDesktop <ericc@ERICDESKTOP-PC>
2024-08-15 10:38:33 -0600

    trying out routes

--- expense-angular/src/app/app.routes.ts
+++ expense-angular/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { EventsComponent } from './events/events.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'view', component: EventsComponent },
+  { path: '', redirectTo: '/view', pathMatch: 'full' } // Optional: Redirect to /view by default
+];
