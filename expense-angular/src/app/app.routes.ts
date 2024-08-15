54ea75df2cf31c63aabfe4132e0580c52c9efe64
EchenDesktop <ericc@ERICDESKTOP-PC>
2024-08-15 10:40:34 -0600

    no default redirect

--- expense-angular/src/app/app.routes.ts
+++ expense-angular/src/app/app.routes.ts
@@ -3,5 +3,5 @@ import { EventsComponent } from './events/events.component';
 
 export const routes: Routes = [
   { path: 'view', component: EventsComponent },
-  { path: '', redirectTo: '/view', pathMatch: 'full' } // Optional: Redirect to /view by default
+  //{ path: '', redirectTo: '/view', pathMatch: 'full' } // Optional: Redirect to /view by default
 ];
