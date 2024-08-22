6abcc37b3d89b26ba3e67b02358a74e47539c8ad
EchenDesktop <ericc@ERICDESKTOP-PC>
2024-08-21 20:18:24 -0600

    removed extra / in prod apiUrl

--- expense-angular/src/environments/environment.ts
+++ expense-angular/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://expense-tracker-flask-backend.vercel.app/'
+    apiUrl: 'https://expense-tracker-flask-backend.vercel.app'
   };
   
